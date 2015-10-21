0d307bb8a17d51a0ab4a41762574c2d337f94ed1
jzy <zhiyongjzy@gmail.com>
2015-10-21 22:38:51 +0800

    Updated update_hosts.py

--- update_hosts.py
+++ update_hosts.py
@@ -1,22 +1,30 @@
 #!/usr/bin/evn python2
+
 import urllib2
 import re
-url = 'http://www.360kb.com/kb/2_122.html'
+url = 'http://www.360kb.com/kb/2_150.html'
 response = urllib2.urlopen(url)
 body = response.read()
-list = body.split("=====")
-tmp = list[1]
-tmp = tmp.split("#google hosts")[0]
-origin = """# localhost name resolution is handled within DNS itself.
-#	127.0.0.1       localhost
-#	::1             localhost
+tmp = body.split("#google hosts")[1]
+tmp = tmp.replace("<br />", "")
+print tmp
+header = """# localhost name resolution is handled within DNS itself.
+127.0.0.1       localhost
+::1             localhost
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+
+
 """
+hosts = header + "\n#" + tmp
 
 #fl = open(r'C:\WINDOWS\system32\drivers\etc\hosts', 'w')
-fl = open(r'.\hosts', 'w')
-fl.write(origin)
-#print tmp
-fl.write(tmp)
+fl = open(r'hosts', 'w')
+fl.write(hosts)
 fl.close()
 
 
