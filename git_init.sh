2cbecee5f30f9731696f1d6689dc6f320165cedd
Zhiyong Jiang <zhiyongjzy@gmail.com>
2018-01-23 22:27:06 +0800

    keep arch installed packages

---
+++ git_init.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Zhiyong Jiang"
+git config --global user.email zhiyongjzy@gmail.com
+
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+
+
+git config --list
